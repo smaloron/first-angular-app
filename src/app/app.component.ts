bc3f27912ec9963172704ace4f2748cec05a7eef
seb <contact@smaloron.eu>
2022-01-03 11:53:17 +0100

    liaison avec varaibles scalaires

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,25 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-angular-app';
-  a = 5;
-  b = 2;
-  isHidden = false;
-  name: string;
-  
-  personList: string[] = [];
-  // personList: Array<string> = []
-
-  buttonClick(event: Event): void {
-    this.isHidden = !this.isHidden;
-    console.log(event);
-  }
-
-  setName(name: string): void {
-    this.name = name;
-    this.personList.push(name);
-    console.log(this.personList);
-  }
-
+  userName = 'Seb';
+  userPass = '123';
 
 }
