c0a4c8543a070c9af4ff4e26af5341dc50f558d0
seb <contact@smaloron.eu>
2022-01-03 14:04:24 +0100

    Boucle sur un Array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  user = {
-    name: 'Seb',
-    pass: '123'
-  }
-
-  isFormValid() {
-    return (
-      this.user.name &&
-      this.user.name.length > 0 &&
-      this.user.pass &&
-      this.user.pass.length > 0
-    );
-  }
-
-  validForm() {
-    console.log(this.user);
-  }
+  personList = [
+    { name: 'Brahé', firstName: 'Tycho' },
+    { name: 'Hopper', firstName: 'Grace' },
+    { name: 'Eluard', firstName: 'Paul' },
+  ];
 
 }
